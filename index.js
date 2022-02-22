e531a7741a081f21dfdb51fdd7eb6a34176341d4
nicolepage <npage1129@gmail.com>
2022-02-22 16:46:34 -0700

    commit tree 1b1a57305104a7f39062b157a89ce35730b983f0

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
 
 // Write your code here
+let num1 = 2;
+let num2 = 31;
+let multiply = num1 * num2;
+
+
+let random = Math.floor(Math.random() * 0) + 1;
+
+let num3 = 8;
+let num4 = 2;
+let mod = num3/num4;
+
+
+let max = Math.max(1, 5, 7, 20);
